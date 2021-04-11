b3af6c042cfbacb09a4191eb23e30c1cf72706b3
Григорий Андреевич Васильев <70897555+VasilievGrigory@users.noreply.github.com>
2021-04-11 19:30:10 +0300

    Add files via upload

--- list.cpp
+++ list.cpp
@@ -11,15 +11,16 @@ private:
 	int8_t data[chunkSize];
         node* next;
     };
-
-    bool is_ready = false;
     std::vector<node*> pools_of_memory;
-    node* current_allocate_point = nullptr;
+    node* current_allocate_point;
     size_t  size = 2;
     static const size_t mx_pool_size = 8192;
-
+public:
+    FixedAllocator() {
+        new_memory_pool();
+    }
     void new_memory_pool() {
-	if (size < mx_pool_size) {
+        if (size < mx_pool_size) {
             size *= 2;
         }
         current_allocate_point  = new node[size];
@@ -29,19 +30,10 @@ private:
         }
         current_allocate_point[size - 1].next = nullptr;
     }
-public:
-    FixedAllocator() {
-        new_memory_pool();
-    }
-    FixedAllocator(const FixedAllocator& alloc): is_ready(alloc.is_ready), pools_of_memory(alloc.pools_of_memory), 
-	current_allocate_point(alloc.current_allocate_point), size(alloc.size){}
     void* allocate() {
-	    std::cout<<2;
-        if (!is_ready) {
+        if (!current_allocate_point) {
             new_memory_pool();
-	    is_ready = true;
         }
-	std::cout<<3;
         node*  memory_for_work = current_allocate_point; 
  	current_allocate_point = current_allocate_point->next; 
         return static_cast<void*>(memory_for_work);
@@ -60,34 +52,21 @@ public:
 template <class T>
 class FastAllocator {
 public:
-
     using value_type = T;
-    FastAllocator() = default;
-    ~FastAllocator() = default;
-    FastAllocator(const FastAllocator& alloc):fixedAlloc4(alloc.fixedAlloc4), fixedAlloc8(alloc.fixedAlloc8){}
     FixedAllocator<24> fixedAlloc4;
-    FixedAllocator<8> fixedAlloc8;
+    FastAllocator(){}
     T* allocate(size_t n) {
-	    std::cout<<6<<' ';
         if (n*sizeof(T) == n*24) {
-		std::cout<<1;
             	return static_cast<T*>(fixedAlloc4.allocate());
         }
-	else if(n*sizeof(T) == n*8){
-		std::cout<<4;
-                return static_cast<T*>(fixedAlloc8.allocate());
-	}
         else {
 		std::cout<<sizeof(T);
             	return std::allocator<T>().allocate(n);
         }
     }
     void deallocate(T* ptr, size_t n){
-	    if (n*sizeof(T) == n*4) {
+	    if (n*sizeof(T) == n*24) {
                 fixedAlloc4.deallocate(ptr);
-	    }
-	    else if(n*sizeof(T) == n*8){
-                fixedAlloc8.deallocate(ptr);
 	    }
 	    else{
             	return std::allocator<T>().deallocate(ptr, n);
@@ -96,28 +75,18 @@ public:
 };
  
 int main() {
-    std::list<int> l1;
-    std::list<int,FastAllocator<int>> l2;
+    std::list<long long> l1;
+    std::list<long long,FastAllocator<long long>> l2;
     auto start_time = high_resolution_clock::now();
-    for (int i = 0; i < 15; ++i) {
-        l1.push_back(i);
+    for (int i = 0; i < 1e7; ++i) {
+        l1.push_back(100000000000000);
     }
     auto end_time = high_resolution_clock::now();
     std::cout << duration_cast<milliseconds>(end_time - start_time).count() << "ms" <<'\n';
     auto start_time1 = high_resolution_clock::now();
-    std::cout<<8;
-    for (int i = 0; i < 15; ++i) {
-	    std::cout<<"aaaaaaaaaaaa";
-        l2.push_back(i);
+    for (int i = 0; i < 1e7; ++i) {
+        l2.push_back(100000000000000);
     }
     auto end_time1 = high_resolution_clock::now();
     std::cout << duration_cast<milliseconds>(end_time1 - start_time1).count() << "ms" << std::endl;
-    for(auto it: l1){
-    	std::cout<<it<<' ';
-    }
-    std::cout<<'\n';
-    for(auto it:l2){
-   	std::cout<<it<<' ';
-    }
-
 }
